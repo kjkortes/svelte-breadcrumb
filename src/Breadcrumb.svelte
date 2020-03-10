9d19f18022b576a01ca197ccaf36fc355252fddb
cjcortes <cjcortes@savvysherpa.com>
2020-03-10 00:59:03 -0500

    lineWidth casing and export lineStrokeWidth prop

--- src/Breadcrumb.svelte
+++ src/Breadcrumb.svelte
@@ -4,8 +4,8 @@
 	export let activeColor = '#0066f5';
 	export let defaultColor = '#999';
 	export let circleSize = 12;
-	export let linewidth = 30;
-	let lineStrokeWidth=2;
+	export let lineWidth = 30;
+	export let lineStrokeWidth=2;
 	$:currentStep = activeStep - 1;
 </script>
 
@@ -22,7 +22,7 @@
 				<circle cx="50%" cy="50%" r="50%" fill="{((i <= currentStep )?activeColor:defaultColor)}" />
 				Sorry, your browser does not support inline SVG.  
 			</svg> 
-			<svg height="{circleSize}" width="{linewidth}">
+			<svg height="{circleSize}" width="{lineWidth}">
 				<line x1="0" y1="50%" x2="200" y2="50%" style="stroke:{((i <= currentStep-1 )?activeColor:defaultColor)};stroke-width:{lineStrokeWidth}" />
 				Sorry, your browser does not support inline SVG.
 			</svg>
